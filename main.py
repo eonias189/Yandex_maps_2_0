92e42de9fb4512e7e2044d7aee40d67f533d0aab
eonias <eonias189@yandex.ru>
2023-02-07 17:49:28 +0400

    задание 2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
@@ -12,18 +13,40 @@ class Window(QMainWindow):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.image_name = image_name
+        self.scroll_speed = 2
         if ll is None:
             print('введите координаты (через запятую,без пробела)')
             ll = input()
         self.ll = ll
-        self.spn = '10,10'
+        self.spnx, self.spny = 10, 10
         self.update_image()
 
     def update_image(self):
-        content = tools.get_image(self.ll, spn=self.spn)
+        content = tools.get_image(self.ll, spn=f'{self.spnx},{self.spny}')
         tools.save_image(self.image_name, content)
         self.pixmap = QPixmap(self.image_name)
         self.im.setPixmap(self.pixmap)
+
+    def keyPressEvent(self, event):
+        spnx, spny = self.spnx, self.spny
+        if event.key() == Qt.Key_PageDown:
+            if self.spnx == 90:
+                pass
+            elif (90 // self.scroll_speed) < self.spnx < 90:
+                self.spnx = 90
+            else:
+                self.spnx = self.scroll_speed * self.spnx
+            if self.spny == 90:
+                pass
+            elif (90 // self.scroll_speed) < self.spny < 90:
+                self.spny = 90
+            else:
+                self.spny = self.scroll_speed * self.spny
+        if event.key() == Qt.Key_PageUp:
+            self.spnx = self.spnx / self.scroll_speed
+            self.spny = self.spny / self.scroll_speed
+        if spnx != self.spnx or spny != self.spny:
+            self.update_image()
 
     def closeEvent(self, event):
         tools.delete_image(self.image_name)
